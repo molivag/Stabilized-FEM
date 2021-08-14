bf73f70ebfac3d7cf7a44c03517225ef0a2bd60a
M_OlivaG <marco.oliva@upc.edu>
2021-08-14 02:04:38 +0200

    main_Stokes: Problema SOLUCIONADO, se corrigio el problema relacionado con malloc y era que S_nrhs valia 803 y debia ser 1 pues era un vector columna y no un vector fila. Se declaro Sipiv como dimension(:). Se distribuyeron los deallocate en su respectivo sitio. Se cambio doubleprecision por real(8) solo por estetica. Se agrego el vecotr Solution para que entre ese a lapack en lugar de sobreescribir Sv. FUNCIONANDO AL 100.

--- main_Stokes.f90
+++ main_Stokes.f90
@@ -6,17 +6,15 @@ program Stokes
 
   implicit none
   ! - - - - - - - - - - * * * Variables que se usan aqui en main * * * * * * * - - - - - - - - - -
-  double precision, allocatable, dimension(:,:) :: A_K
-  double precision, allocatable, dimension(:,:) :: N, dN_dxi, dN_deta
-  double precision, allocatable, dimension(:,:) :: Sv
-  real, allocatable, dimension(:,:)             :: Fbcsvp
-  integer                                       :: NoBV, NoBVcol, i, j
-  external                                      :: SLEEP
-  !========== S O L V E R
-  external                                      :: mkl_dgetrfnp, dgetrf, dgetrs
-  integer                                       :: S_m, S_n, S_lda, S_ldb, S_infoLU, S_nrhs , S_infoSOL
-  integer, allocatable, dimension(:,:)          :: S_ipiv
-  character*1                                   :: S_trans
+  real(8), allocatable, dimension(:,:) :: A_K, Sv, Solution, N, dN_dxi, dN_deta
+  real, allocatable, dimension(:,:)    :: Fbcsvp
+  integer                              :: NoBV, NoBVcol, i, j
+  external                             :: SLEEP
+  !========== S O L V E R              
+  external                             :: mkl_dgetrfnp, dgetrf, dgetrs
+  integer                              :: S_m, S_n, S_lda, S_ldb, S_infoLU, S_nrhs , S_infoSOL
+  integer, allocatable, dimension(:) :: S_ipiv
+  character(len = 1)                   :: S_trans
   ! - - - - - - - - - - - - - - - * * * Fin * * * * * * * - - - - - - - - - - - - - - - - 
   
     
@@ -38,11 +36,7 @@ program Stokes
 
   call GlobalK( A_K, dN_dxi, dN_deta)
 
-  ! DEALLOCATE( N)
-  ! DEALLOCATE( dN_dxi)
-  ! DEALLOCATE( dN_deta)
-  
-  allocate(Sv(2*n_nodes+n_pnodes, 1))
+  allocate(Sv(2*n_nodes+n_pnodes, 1), Solution(2*n_nodes+n_pnodes, 1))
   Sv = 0 !initializing source vector (Sv) 
   
   
@@ -51,7 +45,14 @@ program Stokes
   call ApplyBoundCond(NoBV, Fbcsvp, A_K, Sv )
   !Despues de este ultimo call, obtenemos la matriz y vector global con condiciones de frontera
   ! DEALLOCATE( Fbcsvp)
+  
+  Solution = Sv
 
+  DEALLOCATE( N)
+  DEALLOCATE( dN_dxi)
+  DEALLOCATE( dN_deta)
+
+  100 format (900E20.12)
 
   open(unit=700, file='AK.dat', ACTION="write", STATUS="replace ")
   do i=1,2*n_nodes+n_pnodes 
@@ -64,18 +65,21 @@ program Stokes
     write(710, 100) Sv(i,1)
   end do
   close(710)
+
+  DEALLOCATE( Sv )
+  DEALLOCATE( Fbcsvp)
   
   print*,'!==================== S O L V E R (L A P A C K) ====================!'
   S_m   = size(A_K,1)
   S_n   = size(A_K,2)
   S_lda = max(1,size(A_K,1))
-  allocate( S_ipiv( 1, max(1,min(S_m, S_n)) ) )
+  allocate( S_ipiv(max(1,min(S_m, S_n)) ) )
   S_trans = 'N'
-  S_nrhs  = size(Sv,1)
-  S_ldb = max(1,size(Sv,1))
+  S_nrhs  = 1
+  S_ldb   = max(1,size(Sv,1))
   print*,''
   print*,'= = = = = Solver Parameters = = = = = = = = = ='
-  print*,'= shape of S_ipiv  ',shape(S_ipiv)
+  print*,'= shape of S_ipiv              ',shape(S_ipiv)
   print*,'= Leading dimension of A_K     ',S_lda
   print*,'= Leading dimension of Sv      ',S_ldb
   print*,'= Number of right-hand sides   ',S_nrhs
@@ -94,13 +98,13 @@ program Stokes
     print*,'!===THE FACTORIZATION HAS BEEN COMPLETED, BUT U_',S_infoLU, 'IS EXACTLY SINGULAR.'
     print*, 'DIVISION BY 0 WILL OCCUR IF YOU USE THE FACTOR U FOR SOLVING A SYSTEM OF LINEAR EQUATIONS.'
   endif
-  print*, '.'
-  print*, '.'
-  print*, '.'  
+  print*, ' '
+  print*, ' '
+  print*, ' '  
   print*,'!========== SOLVING SYSTEM OF EQUATIONS '
   ! call sleep(2)
-  call dgetrs( S_trans, S_n, S_nrhs, A_K, S_lda, S_ipiv, Sv, S_ldb, S_infoSOL )
-
+  call dgetrs( S_trans, S_n, S_nrhs, A_K, S_lda, S_ipiv, Solution, S_ldb, S_infoSOL )
+  DEALLOCATE( S_ipiv)
   if ( S_infoSOL .eq. 0 ) then
     print*,'!=== SYSTEM SOLVED WITH STATUS', S_infoSOL, ', THE EXECUTION IS SUCCESSFUL.'
   elseif(S_infoSOL .lt. 0 )then
@@ -110,7 +114,6 @@ program Stokes
   print*,' '
   
   ! ========== Escribir en archivo la matriz global
-  100 format (900E20.12)
   open(unit=70, file='AK_LU_old.dat', ACTION="write", STATUS="replace ")
   do i=1,2*n_nodes+n_pnodes 
     write(70, 100)( A_K(i,j) ,j=1,2*n_nodes+n_pnodes)
@@ -119,18 +122,15 @@ program Stokes
 
   open(unit=71, file='Sol_old.dat', ACTION="write", STATUS="replace ")
   do i=1,2*n_nodes+n_pnodes 
-    write(71, 100) Sv(i,1)
+    write(71, 100) Solution(i,1)
   end do
   close(71)
 
-
-  ! DEALLOCATE( N)
-  ! DEALLOCATE( dN_dxi)
-  ! DEALLOCATE( dN_deta)
-  ! DEALLOCATE( Fbcsvp)
-  ! DEALLOCATE( S_ipiv)
-  ! DEALLOCATE( A_K)
-  ! DEALLOCATE( Sv )
+  print*, 'Files written succesfully. . . . .'
+  print*, ' ' 
+  
+  DEALLOCATE( A_K)
+  DEALLOCATE( Solution )
   
 
 end program Stokes
