fd4063a80222a670a29db90fe89f9a635882f8dc
M_OlivaG <marco.oliva@upc.edu>
2021-08-20 23:41:52 +0200

    STOKES: Cambio de nombre a main_Stokes, cambio de nombre para llamada a funciones de forma, se quito los loops para imprimir matrix y vector y se agrego call a writeMatrix, se quitaron la impresion de solver parameters, se quitron los if de info y se agregaron las llamadas a la funcion MKLfactoResult y MKLsolverResult

--- main_Stokes.f90
+++ main_Stokes.f90
@@ -1,76 +1,57 @@
-program Stokes
+program main_Stokes
   use library
   use Parameters
   use Isoparametric
-
 
   implicit none
   ! - - - - - - - - - - * * * Variables que se usan aqui en main * * * * * * * - - - - - - - - - -
   real(8), allocatable, dimension(:,:) :: A_K, Sv, Solution, N, dN_dxi, dN_deta
   real, allocatable, dimension(:,:)    :: Fbcsvp
-  integer                              :: NoBV, NoBVcol, i, j
+  integer                              :: NoBV, NoBVcol
   external                             :: SLEEP
-  !========== S O L V E R              
+  !=============== S O L V E R ===============              
   external                             :: mkl_dgetrfnp, dgetrf, dgetrs
   integer                              :: S_m, S_n, S_lda, S_ldb, S_infoLU, S_nrhs , S_infoSOL
-  integer, allocatable, dimension(:) :: S_ipiv
-  character(len = 1)                   :: S_trans
+  integer, allocatable, dimension(:)   :: S_ipiv
+  character(len=1)                     :: S_trans
   ! - - - - - - - - - - - - - - - * * * Fin * * * * * * * - - - - - - - - - - - - - - - - 
   
   call GeneralInfo( )
-  call ReadIntegerFile(20,"elements.dat", Nelem, nUne + 1, elements)  
-  call ReadRealFile(10,"nodes.dat", n_nodes,3, nodes) !Para dreducir el numero de subrutinas, usar la sentencia option para 
+  call ReadIntegerFile(10,"elements.dat", Nelem, nUne + 1, elements)  
+  call ReadRealFile(20,"nodes.dat", n_nodes,3, nodes) !Para dreducir el numero de subrutinas, usar la sentencia option para 
   call ReadReal(30,"materials.dat", materials)    !Para dreducir el numero de subrutinas, usar la sentencia option para      
   call ReadIntegerFile(40,"pnodes.dat", n_nodes,2, pnodes)
   call ReadIntegerFile(50,"pelements.dat", Nelem,nPne + 1, pelements)
+  print*, ' '
+  print*, '!=============== INFO DURING EXECUTION ===============!'
   
   call GetQuadGauss(ngp,ngp,gauss_points, gauss_weights)
+  call ShapeFunctions(gauss_points, nUne, N, dN_dxi, dN_deta)  
   
-  call Quad8Nodes(gauss_points, N, dN_dxi, dN_deta)
-
   allocate(A_K(2*n_nodes+n_pnodes, 2*n_nodes+n_pnodes))
   call SetBounCond( NoBV, NoBVcol) !Esta funcion crea el archivo bcsVP.dat
   allocate( Fbcsvp(NoBV, NoBVcol) ) !Designo la memoria para la matriz de nodos con valor en la frontera
   call ReadMixFile(60,"Fbcsvp.dat", NoBV, NoBVcol, Fbcsvp)!Llamo el archivo de valores en la frontera y lo guardo en Fbcsvp
   
-
   call GlobalK( A_K, dN_dxi, dN_deta)
 
   allocate(Sv(2*n_nodes+n_pnodes, 1), Solution(2*n_nodes+n_pnodes, 1))
   Sv = 0 !initializing source vector (Sv) 
-  
-  
   !========== Una vez calculada la matriz global y el vector de fuente (Sv), les aplicamos las condiciones
   ! de frontera esta subrutina anterior usa como input Sv y A_K y los entrega de nuevo con las BCS aplicadas 
   call ApplyBoundCond(NoBV, Fbcsvp, A_K, Sv )
   !Despues de este ultimo call, obtenemos la matriz y vector global con condiciones de frontera
-  ! DEALLOCATE( Fbcsvp)
   
-  Solution = Sv
-
+  Solution = Sv !Solucion sera reescrito por la solucion de lapack asi no reescribo el vector global.
   DEALLOCATE( N)
   DEALLOCATE( dN_dxi)
   DEALLOCATE( dN_deta)
-
-  100 format (900E20.12)
-
-  open(unit=700, file='AK.dat', ACTION="write", STATUS="replace ")
-  do i=1,2*n_nodes+n_pnodes 
-    write(700, 100)( A_K(i,j) ,j=1,2*n_nodes+n_pnodes)
-  end do
-  close(700)
-
-  open(unit=710, file='Source.dat', ACTION="write", STATUS="replace ")
-  do i=1,2*n_nodes+n_pnodes 
-    write(710, 100) Sv(i,1)
-  end do
-  close(710)
-
-  DEALLOCATE( Sv )
   DEALLOCATE( Fbcsvp)
+  call writeMatrix(A_K, 111, 'Global_K.dat', Sv, 222, 'Global_Sv.dat') !write global matrix and vector
+  DEALLOCATE( Sv )
   
   print*,' '
-  print*,'!==================== S O L V E R (L A P A C K) ====================!'
+  print*,'!=============== SOLVER (LAPACK) ===============!'
   S_m   = size(A_K,1)
   S_n   = size(A_K,2)
   S_lda = max(1,size(A_K,1)) ! lda ≥ max(1, n).
@@ -78,64 +59,25 @@ program Stokes
   S_trans = 'N'
   S_nrhs  = 1
   S_ldb   = max(1,size(Sv,1))
-  print*,''
-  print*,'= = = = = Solver Parameters = = = = = = = = = ='
-  print*,'= shape of S_ipiv              ',shape(S_ipiv)
-  print*,'= Leading dimension of A_K     ',S_lda
-  print*,'= Leading dimension of Sv      ',S_ldb
-  print*,'= Number of right-hand sides   ',S_nrhs
-  print*,'= = = = = = = = = = = = = = = = = = = = = = = ='
-  print*,''
-  
-  print*,'!=== INITIALIZING LU FACTORIZATION A = P*L*U'
-  ! call sleep(2)
+ 
+  print*,'  •INITIALIZING LU FACTORIZATION A = P*L*U'
   call dgetrf( S_m, S_n, A_K, S_lda, S_ipiv, S_infoLU )
   ! call mkl_dgetrfnp( S_m, S_n, A_K, S_lda, S_infoLU )
-  if ( S_infoLU .eq. 0 ) then
-    print*,'!===FACTORIZATION DONE WITH STATUS', S_infoLU, ', THE EXECUTION IS SUCCESSFUL.'
-  elseif(S_infoLU .lt. 0 )then
-    print*,'!===MATRIX FACTORIZED WITH STATUS', S_infoLU, 'THE',S_infoLU,'-TH PARAMETER HAD AN ILLEGAL VALUE.'
-  elseif(S_infoLU .gt. 0 )then
-    print*,'!===THE FACTORIZATION HAS BEEN COMPLETED, BUT U_',S_infoLU, 'IS EXACTLY SINGULAR.'
-    print*, 'DIVISION BY 0 WILL OCCUR IF YOU USE THE FACTOR U FOR SOLVING A SYSTEM OF LINEAR EQUATIONS.'
-  endif
-  print*, ' '
-  print*, ' '
+  call MKLfactoResult( S_infoLU )
+
   print*, ' '  
-  print*,'!========== SOLVING SYSTEM OF EQUATIONS '
-  ! call sleep(2)
+  print*,'  •SOLVING SYSTEM OF EQUATIONS '
   call dgetrs( S_trans, S_n, S_nrhs, A_K, S_lda, S_ipiv, Solution, S_ldb, S_infoSOL )
-  
   ! call dgesv( n, nrhs, a, lda, ipiv, b, ldb, info ) !other option from page 765 lapack manual
+  call MKLsolverResult( S_infoSOL )
   
   DEALLOCATE( S_ipiv)
-
-  if ( S_infoSOL .eq. 0 ) then
-    print*,'!=== SYSTEM SOLVED WITH STATUS', S_infoSOL, ', THE EXECUTION IS SUCCESSFUL.'
-  elseif(S_infoSOL .lt. 0 )then
-    print*,'!=== SYSTEM SOLVED WITH STATUS', S_infoSOL, 'THE',S_infoSOL,'-TH PARAMETER HAD AN ILLEGAL VALUE.'
-  endif
-  ! call sleep(1)
-  print*,' '
-  
-  ! ========== Escribir en archivo la matriz global
-  open(unit=70, file='AK_LU_old.dat', ACTION="write", STATUS="replace ")
-  do i=1,2*n_nodes+n_pnodes 
-    write(70, 100)( A_K(i,j) ,j=1,2*n_nodes+n_pnodes)
-  end do
-  close(70)
-
-  open(unit=71, file='Sol_old.dat', ACTION="write", STATUS="replace ")
-  do i=1,2*n_nodes+n_pnodes 
-    write(71, 100) Solution(i,1)
-  end do
-  close(71)
-
+  call writeMatrix(A_K, 333, 'AK_LU.dat', Solution, 444, 'Solution.dat')
+  DEALLOCATE( A_K)
+  DEALLOCATE( Solution)
+  print*,' '  
   print*, 'Files written succesfully. . . . .'
   print*, ' ' 
-  
-  DEALLOCATE( A_K)
-  DEALLOCATE( Solution )
-  
+   
 
-end program Stokes
+end program main_Stokes
