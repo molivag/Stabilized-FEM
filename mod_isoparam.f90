d3505df697a78e292fa44a5592cf9bef72b51230
M_OlivaG <marco.oliva@upc.edu>
2021-08-20 23:41:07 +0200

    ISOPARAM: En ShapeFunctions se acomodo dN_dxi, dN_deta y N lo que SOLUCIONO el problema de producir el vector de fuente de mucha mayor magnitud comparado con matlab.

--- mod_isoparam.f90
+++ mod_isoparam.f90
@@ -292,204 +292,204 @@ module Isoparametric
     end subroutine Triang3pElem
 
     ! = = = = = = = = = = = = = = = = = = = = =  = = = = = = 
-    ! subroutine ShapeFunctions(gauss_points, Nne, totGp,  N, dN_dxi, dN_deta )
-    !   implicit None
+    subroutine ShapeFunctions2(gauss_points, Nne, totGp,  N, dN_dxi, dN_deta )
+      implicit None
 
-    !   integer, intent(in) :: Nne, totGp
-    !   double precision, dimension(:,:), intent(in)               :: gauss_points
-    !   double precision, allocatable, dimension(:,:), intent(out) :: N
-    !   double precision, allocatable, dimension(:,:), intent(out), optional :: dN_dxi, dN_deta
-    !   double precision, dimension(totGp) :: xi_vector, eta_vector
-    !   integer, dimension(Nne,DimPr)                     :: master_nodes
-    !   double precision                                  :: xi, eta, mn_xi, mn_eta
-    !   integer                                           :: i, j, jj, k
+      integer, intent(in) :: Nne, totGp
+      double precision, dimension(:,:), intent(in)               :: gauss_points
+      double precision, allocatable, dimension(:,:), intent(out) :: N
+      double precision, allocatable, dimension(:,:), intent(out), optional :: dN_dxi, dN_deta
+      double precision, dimension(totGp) :: xi_vector, eta_vector
+      integer, dimension(Nne,DimPr)                     :: master_nodes
+      double precision                                  :: xi, eta, mn_xi, mn_eta
+      integer                                           :: i, j, jj, k
       
-    !   ! = = = = = = = = = = = = = = = = = = = = = = = = = = =
+      ! = = = = = = = = = = = = = = = = = = = = = = = = = = =
     
 
-    !   xi_vector  = gauss_points(:,1)     ! xi-coordinate of point j
-    !   eta_vector = gauss_points(:,2)
+      xi_vector  = gauss_points(:,1)     ! xi-coordinate of point j
+      eta_vector = gauss_points(:,2)
 
-    !   100 format (3A, 1x, I1, 1x, A)      
+      100 format (3A, 1x, I1, 1x, A)      
 
-    !   select case(ElemType)
+      select case(ElemType)
 
-    !     CASE ('Quad')
-    !       select case(nne)
-    !       case  (8)
-    !         write(*,100) 'Element type: ', ElemType, 'whit', nne, 'nodes per element'
-    !        ! Shape functions for square (quadrilaters) linear elements
-    !        !
-    !        !  |
-    !        !  |
-    !        !  | o- - o - -o
-    !        !  Y |         |
-    !        !  | o         o
-    !        !  | |         |
-    !        !  | o- - o - -o
-    !        !  |
-    !        !  +--------X-------->
+        CASE ('Quad')
+          select case(nne)
+          case  (8)
+            write(*,100) 'Element type: ', ElemType, 'whit', nne, 'nodes per element'
+           ! Shape functions for square (quadrilaters) linear elements
+           !
+           !  |
+           !  |
+           !  | o- - o - -o
+           !  Y |         |
+           !  | o         o
+           !  | |         |
+           !  | o- - o - -o
+           !  |
+           !  +--------X-------->
 
-    !         !coordinates of the nodes of the master element
-    !         master_nodes = reshape([1, -1, -1, 1, 0, -1, 0, 1, 1, 1, -1, -1, 1, 0, -1, 0], [Nne,DimPr])
-    !         !NOTA ** Para que el reshape funcione correctamente, o que produzca el par de valores deseado, primero se deben
-    !         !colocar todos los valores en x, luego todos los de y y luego, si hubiera todos los de z para que al acomodarse salga el par
-    !         !suponiendo un reshape de 3,2 debe acomodarse x1, x2, x3, y1, y2, y3 DUDA *Siempre es asi*
-    !         ! dN(xi,eta)/dx = dN/dxi(dxi\dx) + dN/deta(deta/dx)
-    !         ! dN(xi,eta)/dy = dN/dxi(dxi\dy) + dN/deta(deta/dy)
-    !         ! Aqui se calculan as funciones de forma N y parte de las derivadas dN/dxi and dN_deta
-    !         ! mas no las derivadas dN/dx and dN/dy completas
+            !coordinates of the nodes of the master element
+            master_nodes = reshape([1, -1, -1, 1, 0, -1, 0, 1, 1, 1, -1, -1, 1, 0, -1, 0], [Nne,DimPr])
+            !NOTA ** Para que el reshape funcione correctamente, o que produzca el par de valores deseado, primero se deben
+            !colocar todos los valores en x, luego todos los de y y luego, si hubiera todos los de z para que al acomodarse salga el par
+            !suponiendo un reshape de 3,2 debe acomodarse x1, x2, x3, y1, y2, y3 DUDA *Siempre es asi*
+            ! dN(xi,eta)/dx = dN/dxi(dxi\dx) + dN/deta(deta/dx)
+            ! dN(xi,eta)/dy = dN/dxi(dxi\dy) + dN/deta(deta/dy)
+            ! Aqui se calculan as funciones de forma N y parte de las derivadas dN/dxi and dN_deta
+            ! mas no las derivadas dN/dx and dN/dy completas
 
-    !         if (present(dN_dxi) .and. present(dN_deta))then
-    !           write(*,*) 'Escribo solo derivadas de funciones base'
-    !           allocate( N(Nne,totGp) )
-    !           allocate(dN_dxi(Nne,totGp) )
-    !           allocate(dN_deta(Nne,totGp))
-    !           N = 0.0
-    !           dN_dxi  = 0.0
-    !           dN_deta = 0.0
-    !           do j = 1, totGp
-    !             xi  = xi_vector(j)      ! xi-coordinate of point j
-    !             eta = eta_vector(j)     ! eta-coordinate of point j
-    !             N(5,j)=1.0/2*(1-xi**2)*(1+eta)
-    !             N(6,j)=1.0/2*(1-xi)*(1-eta**2)
-    !             N(7,j)=1.0/2*(1-xi**2)*(1-eta)
-    !             N(8,j)=1.0/2*(1+xi)*(1-eta**2)
-    !             dN_dxi(5,j)=-xi*(1+eta)
-    !             dN_dxi(6,j)=-1.0/2*(1-eta**2)
-    !             dN_dxi(7,j)=-xi*(1-eta)
-    !             dN_dxi(8,j)=1.0/2*(1-eta**2)
-    !             dN_deta(5,j)=1.0/2*(1-xi**2)
-    !             dN_deta(6,j)=(1-xi)*(-eta)
-    !             dN_deta(7,j)=-1.0/2*(1-xi**2)
-    !             dN_deta(8,j)=(1+xi)*(-eta)
+            if (present(dN_dxi) .and. present(dN_deta))then
+              write(*,*) 'Escribo solo derivadas de funciones base'
+              allocate( N(Nne,totGp) )
+              allocate(dN_dxi(Nne,totGp) )
+              allocate(dN_deta(Nne,totGp))
+              N = 0.0
+              dN_dxi  = 0.0
+              dN_deta = 0.0
+              do j = 1, totGp
+                xi  = xi_vector(j)      ! xi-coordinate of point j
+                eta = eta_vector(j)     ! eta-coordinate of point j
+                N(5,j)=1.0/2*(1-xi**2)*(1+eta)
+                N(6,j)=1.0/2*(1-xi)*(1-eta**2)
+                N(7,j)=1.0/2*(1-xi**2)*(1-eta)
+                N(8,j)=1.0/2*(1+xi)*(1-eta**2)
+                dN_dxi(5,j)=-xi*(1+eta)
+                dN_dxi(6,j)=-1.0/2*(1-eta**2)
+                dN_dxi(7,j)=-xi*(1-eta)
+                dN_dxi(8,j)=1.0/2*(1-eta**2)
+                dN_deta(5,j)=1.0/2*(1-xi**2)
+                dN_deta(6,j)=(1-xi)*(-eta)
+                dN_deta(7,j)=-1.0/2*(1-xi**2)
+                dN_deta(8,j)=(1+xi)*(-eta)
 
-    !             do i = 1, 4
-    !               mn_xi = master_nodes(i,1)
-    !               mn_eta= master_nodes(i,2)
-    !               if (i==1) then
-    !                 jj=8
-    !               else
-    !                 jj=i+3
-    !               end if
-    !               k=i+4
-    !               N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0 - 1.0/2*(N(jj,j)+N(k,j))
-    !               dN_dxi(i,j)= mn_xi*(1.0 + mn_eta*eta)/4.0 - 1.0/2*(dN_dxi(jj,j)+dN_dxi(k,j))
-    !               dN_deta(i,j)= mn_eta*(1.0 + mn_xi*xi)/4.0 - 1.0/2*(dN_deta(jj,j)+dN_deta(k,j))
-    !             end do
-    !           end do
-    !         else
-    !           write(*,*) 'Escribo solo funciones base'
-    !           allocate( N(Nne,totGp) )
-    !           N = 0.0
-    !           do j = 1, totGp
-    !             xi  = xi_vector(j)      ! xi-coordinate of point j
-    !             eta = eta_vector(j)     ! eta-coordinate of point j
-    !             N(5,j)=1.0/2*(1-xi**2)*(1+eta)
-    !             N(6,j)=1.0/2*(1-xi)*(1-eta**2)
-    !             N(7,j)=1.0/2*(1-xi**2)*(1-eta)
-    !             N(8,j)=1.0/2*(1+xi)*(1-eta**2)
-    !             do i = 1, 4
-    !               mn_xi = master_nodes(i,1)
-    !               mn_eta= master_nodes(i,2)
-    !               if (i==1) then
-    !                 jj=8
-    !               else
-    !                 jj=i+3
-    !               end if
-    !               k=i+4
-    !               N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0 - 1.0/2*(N(jj,j)+N(k,j))
-    !             end do
-    !           end do
-    !         end if
-    !       case  (4)
-    !        !  Bilinear shape functions 
+                do i = 1, 4
+                  mn_xi = master_nodes(i,1)
+                  mn_eta= master_nodes(i,2)
+                  if (i==1) then
+                    jj=8
+                  else
+                    jj=i+3
+                  end if
+                  k=i+4
+                  N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0 - 1.0/2*(N(jj,j)+N(k,j))
+                  dN_dxi(i,j)= mn_xi*(1.0 + mn_eta*eta)/4.0 - 1.0/2*(dN_dxi(jj,j)+dN_dxi(k,j))
+                  dN_deta(i,j)= mn_eta*(1.0 + mn_xi*xi)/4.0 - 1.0/2*(dN_deta(jj,j)+dN_deta(k,j))
+                end do
+              end do
+            else
+              write(*,*) 'Escribo solo funciones base'
+              allocate( N(Nne,totGp) )
+              N = 0.0
+              do j = 1, totGp
+                xi  = xi_vector(j)      ! xi-coordinate of point j
+                eta = eta_vector(j)     ! eta-coordinate of point j
+                N(5,j)=1.0/2*(1-xi**2)*(1+eta)
+                N(6,j)=1.0/2*(1-xi)*(1-eta**2)
+                N(7,j)=1.0/2*(1-xi**2)*(1-eta)
+                N(8,j)=1.0/2*(1+xi)*(1-eta**2)
+                do i = 1, 4
+                  mn_xi = master_nodes(i,1)
+                  mn_eta= master_nodes(i,2)
+                  if (i==1) then
+                    jj=8
+                  else
+                    jj=i+3
+                  end if
+                  k=i+4
+                  N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0 - 1.0/2*(N(jj,j)+N(k,j))
+                end do
+              end do
+            end if
+          case  (4)
+           !  Bilinear shape functions 
               
-    !        !  |
-    !        !  | o- - - -o
-    !        !  Y |       |
-    !        !  | |       |
-    !        !  | |       |
-    !        !  | o- - - -o
-    !        !  |
-    !        !  +- - - x - - ->
+           !  |
+           !  | o- - - -o
+           !  Y |       |
+           !  | |       |
+           !  | |       |
+           !  | o- - - -o
+           !  |
+           !  +- - - x - - ->
 
-    !         write(*,100) 'Element type: ', ElemType, 'whit', nne, 'nodes per element'
-    !         !do loop: compute N, dN_dxi, dN_deta
-    !         if (present(dN_dxi) .and. present(dN_deta))then
-    !           write(*,*) 'Escribo solo derivadas de funciones base de 4 nodos'
-    !           allocate(N(Nne,totGp) )
-    !           allocate(dN_dxi(Nne,totGp) )
-    !           allocate(dN_deta(Nne,totGp) )
-    !           N = 0.0
-    !           dN_dxi  = 0.0
-    !           dN_deta = 0.0
+            write(*,100) 'Element type: ', ElemType, 'whit', nne, 'nodes per element'
+            !do loop: compute N, dN_dxi, dN_deta
+            if (present(dN_dxi) .and. present(dN_deta))then
+              write(*,*) 'Escribo solo derivadas de funciones base de 4 nodos'
+              allocate(N(Nne,totGp) )
+              allocate(dN_dxi(Nne,totGp) )
+              allocate(dN_deta(Nne,totGp) )
+              N = 0.0
+              dN_dxi  = 0.0
+              dN_deta = 0.0
 
-    !           do j=1,totGp                            ! columns for point 1,2 ...
-    !             xi=xi_vector(j);                      ! xi-coordinate of point j 
-    !             eta=eta_vector(j);                    ! eta-coordinate of point j 
-    !             do i=1,4                              ! rows for N1, N2, ...
-    !               mn_xi = master_nodes(i,1)
-    !               mn_eta= master_nodes(i,2)
-    !               N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0
-    !               dN_dxi(i,j)= mn_xi*(1.0 + mn_eta*eta)/4.0             ! dNi/dxi(xi,eta)
-    !               dN_deta(i,j)= mn_eta*(1.0 + mn_xi*xi )/4.0            ! dNi/deta(xi,eta
-    !             end do
-    !           end do
-    !         else
-    !           allocate(N(Nne,totGp) )
-    !           N     = 0.0
-    !           write(*,*) 'Escribo funciones base de 4 nodos'
-    !           do j=1,totGp                            ! columns for point 1,2 ...
-    !             xi=xi_vector(j);                      ! xi-coordinate of point j 
-    !             eta=eta_vector(j);                    ! eta-coordinate of point j 
-    !             do i=1,4                              ! rows for N1, N2, ...
-    !               mn_xi = master_nodes(i,1)
-    !               mn_eta= master_nodes(i,2)
-    !               N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0        ! Ni(xi,eta)
-    !             end do
-    !           end do
-    !         endif
-    !       case DEFAULT
-    !         write(*,*) 'Invalid number of nodes in the element.'
-    !       end select
+              do j=1,totGp                            ! columns for point 1,2 ...
+                xi=xi_vector(j);                      ! xi-coordinate of point j 
+                eta=eta_vector(j);                    ! eta-coordinate of point j 
+                do i=1,4                              ! rows for N1, N2, ...
+                  mn_xi = master_nodes(i,1)
+                  mn_eta= master_nodes(i,2)
+                  N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0
+                  dN_dxi(i,j)= mn_xi*(1.0 + mn_eta*eta)/4.0             ! dNi/dxi(xi,eta)
+                  dN_deta(i,j)= mn_eta*(1.0 + mn_xi*xi )/4.0            ! dNi/deta(xi,eta
+                end do
+              end do
+            else
+              allocate(N(Nne,totGp) )
+              N     = 0.0
+              write(*,*) 'Escribo funciones base de 4 nodos'
+              do j=1,totGp                            ! columns for point 1,2 ...
+                xi=xi_vector(j);                      ! xi-coordinate of point j 
+                eta=eta_vector(j);                    ! eta-coordinate of point j 
+                do i=1,4                              ! rows for N1, N2, ...
+                  mn_xi = master_nodes(i,1)
+                  mn_eta= master_nodes(i,2)
+                  N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0        ! Ni(xi,eta)
+                end do
+              end do
+            endif
+          case DEFAULT
+            write(*,*) 'Invalid number of nodes in the element.'
+          end select
 
-    !     CASE ('Trian')
-		! 	    !  |
-		! 	    !  |        o
-		! 	    !  |       / \
-		! 	    !  |      /   \
-		! 	    !  Y     /     \
-		! 	    !  |    /       \
-		! 	    !  |   /         \
-		! 	    !  |  o-----------o
-		! 	    !  |
-		! 	    !  +--------X-------->
-    !       allocate(dN_dxi(Nne,totGp) )
-    !       allocate(dN_deta(Nne,totGp) )
+        CASE ('Trian')
+			    !  |
+			    !  |        o
+			    !  |       / \
+			    !  |      /   \
+			    !  Y     /     \
+			    !  |    /       \
+			    !  |   /         \
+			    !  |  o-----------o
+			    !  |
+			    !  +--------X-------->
+          allocate(dN_dxi(Nne,totGp) )
+          allocate(dN_deta(Nne,totGp) )
 
-    !       dN_dxi  = 0.0
-    !       dN_deta = 0.0
-    !       xi_vector  = gauss_points(:,1)     ! xi-coordinate of point j
-    !       eta_vector = gauss_points(:,2)
-    !       do j=1,totGp
-    !         xi=xi_vector(j);                      ! xi-coordinate of point j 
-    !         eta=eta_vector(j); 
-    !         N(1,j)      =  2*(1-xi-eta)*(1-xi-eta-0.5)
-    !         dN_dxi(1,j) = -2*(1-xi-eta-0.5)-2*(1-xi-eta) 
-    !         dN_deta(1,j)= -2*(1-xi-eta-0.5)-2*(1-xi-eta) 
-    !         N(2,j)      = 2*xi*(xi-0.5)
-    !         dN_dxi(2,j) = 2*(xi-0.5) + 2*xi 
-    !         dN_deta(2,j)= 0
-    !         N(3,j)      =  2*eta*(eta-0.5)
-    !         dN_dxi(3,j) = 0
-    !         dN_deta(3,j)= 2*(eta-0.5) + 2*eta 
-    !       end do
+          dN_dxi  = 0.0
+          dN_deta = 0.0
+          xi_vector  = gauss_points(:,1)     ! xi-coordinate of point j
+          eta_vector = gauss_points(:,2)
+          do j=1,totGp
+            xi=xi_vector(j);                      ! xi-coordinate of point j 
+            eta=eta_vector(j); 
+            N(1,j)      =  2*(1-xi-eta)*(1-xi-eta-0.5)
+            dN_dxi(1,j) = -2*(1-xi-eta-0.5)-2*(1-xi-eta) 
+            dN_deta(1,j)= -2*(1-xi-eta-0.5)-2*(1-xi-eta) 
+            N(2,j)      = 2*xi*(xi-0.5)
+            dN_dxi(2,j) = 2*(xi-0.5) + 2*xi 
+            dN_deta(2,j)= 0
+            N(3,j)      =  2*eta*(eta-0.5)
+            dN_dxi(3,j) = 0
+            dN_deta(3,j)= 2*(eta-0.5) + 2*eta 
+          end do
         
-    !     case DEFAULT
-    !       write(*,*) 'Invalid type of element.'
-    !   end select
-    ! end subroutine ShapeFunctions
+        case DEFAULT
+          write(*,*) 'Invalid type of element.'
+      end select
+    end subroutine ShapeFunctions2
 
 
     !  ==============================
@@ -513,8 +513,6 @@ module Isoparametric
       N = 0.0
       xi_vector  = gauss_points(:,1)     ! xi-coordinate of point j
       eta_vector = gauss_points(:,2)
-
-      
 
       select case(ElemType)
 
@@ -553,6 +551,16 @@ module Isoparametric
               do j = 1, totGp
                 xi  = xi_vector(j)      ! xi-coordinate of point j
                 eta = eta_vector(j)     ! eta-coordinate of point j
+              
+                dN_dxi(5,j)=-xi*(1+eta)
+                dN_dxi(6,j)=-1.0/2*(1-eta**2)
+                dN_dxi(7,j)=-xi*(1-eta)
+                dN_dxi(8,j)=1.0/2*(1-eta**2)
+                dN_deta(5,j)=1.0/2*(1-xi**2)
+                dN_deta(6,j)=(1-xi)*(-eta)
+                dN_deta(7,j)=-1.0/2*(1-xi**2)
+                dN_deta(8,j)=(1+xi)*(-eta)
+              
                 do i = 1, 4
                   mn_xi = master_nodes(i,1)
                   mn_eta= master_nodes(i,2)
@@ -564,15 +572,9 @@ module Isoparametric
                   k=i+4
                   dN_dxi(i,j)= mn_xi*(1.0 + mn_eta*eta)/4.0 - 1.0/2*(dN_dxi(jj,j)+dN_dxi(k,j))
                   dN_deta(i,j)= mn_eta*(1.0 + mn_xi*xi)/4.0 - 1.0/2*(dN_deta(jj,j)+dN_deta(k,j))
+              
                 end do
-                dN_dxi(5,j) = -xi*(1+eta)
-                dN_dxi(6,j) = -1.0/2*(1-eta**2)
-                dN_dxi(7,j) = -xi*(1-eta)
-                dN_dxi(8,j) = 1.0/2*(1-eta**2)
-                dN_deta(5,j)= 1.0/2*(1-xi**2)
-                dN_deta(6,j)= (1-xi)*(-eta)
-                dN_deta(7,j)= -1.0/2*(1-xi**2)
-                dN_deta(8,j)= (1+xi)*(-eta)
+              
               end do
             else
               write(*,*) 'Using only shape functions'
@@ -584,6 +586,10 @@ module Isoparametric
             do j = 1, totGp
               xi  = xi_vector(j)      ! xi-coordinate of point j
               eta = eta_vector(j)     ! eta-coordinate of point j
+              N(5,j)=1.0/2*(1-xi**2)*(1+eta)
+              N(6,j)=1.0/2*(1-xi)*(1-eta**2)
+              N(7,j)=1.0/2*(1-xi**2)*(1-eta)
+              N(8,j)=1.0/2*(1+xi)*(1-eta**2)
               do i = 1, 4
                 mn_xi = master_nodes(i,1)
                 mn_eta= master_nodes(i,2)
@@ -595,10 +601,6 @@ module Isoparametric
                 k=i+4
                 N(i,j)=(1.0 + mn_xi*xi)*(1.0 + mn_eta*eta)/4.0 - 1.0/2*(N(jj,j)+N(k,j))
               end do
-              N(5,j) = 1.0/2*(1-xi**2)*(1+eta)
-              N(6,j) = 1.0/2*(1-xi)*(1-eta**2)
-              N(7,j) = 1.0/2*(1-xi**2)*(1-eta)
-              N(8,j) = 1.0/2*(1+xi)*(1-eta**2)
             end do
 
           case  (4)
