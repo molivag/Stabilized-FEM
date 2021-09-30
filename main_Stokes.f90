65a334e2c0d2f30aabcb9f1f67b8bc3dbc195b52
M_OlivaG <marco.oliva@upc.edu>
2021-09-30 13:48:40 +0200

    MAIN: Se agrego una variable para guardar la factorizacion LU y se puso esta como variable de entrada en funciones LAPACK. Esto permite una sola llamada de escritura para A_K y Solution.

--- main_Stokes.f90
+++ main_Stokes.f90
@@ -5,7 +5,7 @@ program main_Stokes
 
   implicit none
   ! - - - - - - - - - - * * * Variables que se usan aqui en main * * * * * * * - - - - - - - - - -
-  real(8), allocatable, dimension(:,:) :: A_K, Sv, Solution, N, dN_dxi, dN_deta
+  real(8), allocatable, dimension(:,:) :: A_K, Sv, AK_LU, Solution, N, dN_dxi, dN_deta
   real(4), allocatable, dimension(:,:) :: Fbcsvp
   integer                              :: NoBV, NoBVcol
   !character(len=20)                    :: FileName
@@ -17,19 +17,18 @@ program main_Stokes
   ! - - - - - - - - - - - - - - - * * * Fin * * * * * * * - - - - - - - - - - - - - - - - 
   
   call GeneralInfo( )
-  call ReadIntegerFile(10,"elementsQ1.dat", Nelem, nUne + 1, elements)  
-  call ReadRealFile(20,"nodesQ1.dat", n_nodes,3, nodes) !Para dreducir el numero de subrutinas, usar la sentencia option para 
+  call ReadIntegerFile(10,"elementsP1P1.dat", Nelem, nUne + 1, elements)  
+  call ReadRealFile(20,"nodesP1P1.dat", n_nodes,3, nodes) !Para dreducir el numero de subrutinas, usar la sentencia option para 
   call ReadReal(30,"materials.dat", materials)    !Para dreducir el numero de subrutinas, usar la sentencia option para      
-  call ReadIntegerFile(40,"pnodesQ1.dat", n_nodes,2, pnodes)
-  call ReadIntegerFile(50,"elementsQ1.dat", Nelem,nPne + 1, pelements)
+  call ReadIntegerFile(40,"pnodesP1P1.dat", n_nodes,2, pnodes)
+  call ReadIntegerFile(50,"elementsP1P1.dat", Nelem,nPne + 1, pelements)
   print*, ' '
   print*, '!=============== INFO DURING EXECUTION ===============!'
   
-  call GetQuadGauss(ngp,ngp,gauss_points, gauss_weights)
-  !call GetTriGauss(gauss_points, gauss_weights)
+  call GaussQuadrature(gauss_points, gauss_weights)
   call ShapeFunctions(gauss_points, nUne, N, dN_dxi, dN_deta)  
   
-  allocate(A_K(2*n_nodes+n_pnodes, 2*n_nodes+n_pnodes))
+  allocate(A_K(2*n_nodes+n_pnodes, 2*n_nodes+n_pnodes), AK_LU(2*n_nodes+n_pnodes, 2*n_nodes+n_pnodes) )
   call SetBounCond( NoBV, NoBVcol) !Esta funcion crea el archivo bcsVP.dat
   allocate( Fbcsvp(NoBV, NoBVcol) ) !Designo la memoria para la matriz de nodos con valor en la frontera
   call ReadMixFile(60,"Fbcsvp.dat", NoBV, NoBVcol, Fbcsvp)!Llamo el archivo de valores en la frontera y lo guardo en Fbcsvp
@@ -44,50 +43,49 @@ program main_Stokes
   !Despues de este ultimo call, obtenemos la matriz y vector global con condiciones de frontera
   
   Solution = Sv !Solucion sera reescrito por la solucion de lapack asi no reescribo el vector global.
+  AK_LU = A_K
   DEALLOCATE( N)
   DEALLOCATE( dN_dxi)
   DEALLOCATE( dN_deta)
   DEALLOCATE( Fbcsvp)
-  !call writeMatrix(A_K, 111, 'Stab_GlobalK.txt', Sv, 222, 'Stab_GlobalSv.txt') !write global matrix and vector
   DEALLOCATE( Sv )
   
   print*,' '
   print*,'!=============== SOLVER (LAPACK) ===============!'
-  S_m   = size(A_K,1)
-  S_n   = size(A_K,2)
-  S_lda = max(1,size(A_K,1)) ! lda ≥ max(1, n).
+  S_m   = size(AK_LU,1)
+  S_n   = size(AK_LU,2)
+  S_lda = max(1,size(AK_LU,1)) ! lda ≥ max(1, n).
   allocate( S_ipiv(max(1,min(S_m, S_n)) ) )
   S_trans = 'N'
   S_nrhs  = 1
-  S_ldb   = max(1,size(Sv,1))
+  S_ldb   = max(1,size(Solution,1))
  
   print*,'  •INITIALIZING LU FACTORIZATION A = P*L*U'
-  call dgetrf( S_m, S_n, A_K, S_lda, S_ipiv, S_infoLU )
-  ! call mkl_dgetrfnp( S_m, S_n, A_K, S_lda, S_infoLU )
+  call dgetrf( S_m, S_n, AK_LU, S_lda, S_ipiv, S_infoLU )
   call MKLfactoResult( S_infoLU )
 
   print*, ' '  
   print*,'  •SOLVING SYSTEM OF EQUATIONS '
-  call dgetrs( S_trans, S_n, S_nrhs, A_K, S_lda, S_ipiv, Solution, S_ldb, S_infoSOL )
-  ! call dgesv( n, nrhs, a, lda, ipiv, b, ldb, info ) !other option from page 765 lapack manual
+  call dgetrs( S_trans, S_n, S_nrhs, AK_LU, S_lda, S_ipiv, Solution, S_ldb, S_infoSOL )
   call MKLsolverResult( S_infoSOL )
   
   DEALLOCATE( S_ipiv)
-  !call writeMatrix(A_K, 333, 'StabA3K_LU.txt', Solution, 444, 'SolQ1.txt')
-
+  
+  call writeMatrix(A_K, 111, 'GlobalK_Triangle.dat', Solution, 444, 'P1P1Sol.dat')
   !write(*,"(A)") 'Name for mesh file...'
   !read(*,"(A)") FileName 
-  call PosProcess(Solution, 'StokesQ1.post.msh', 'msh')
-
+  call PosProcess(Solution, 'StokesP1P1.post.msh', 'msh')
   !write(*,"(A)") 'Name for results file...'
   !read(*,"(A)") FileName 
-  call PosProcess(Solution, 'StokesQ1.post.res', 'res')
+  call PosProcess(Solution, 'StokesP1P1.post.res', 'res')
 
   DEALLOCATE( A_K)
+  DEALLOCATE( AK_LU)
   DEALLOCATE( Solution)
   print*,' '  
-  print*, 'Files written succesfully on Res/ . . . . .'
   
+  print*, 'Files .post written succesfully on Pos/ . . . . .'
+  print*, 'Files .dat written succesfully on Res/ . . . . .'
   print*, ' ' 
 
 end program main_Stokes
